5c05bfce08edeb931d9a58d62ec5359dbf9bd302
Danny Vaca <danny270793@icloud.com>
2026-07-16 20:07:11 -0500

    Initial Commit

---
+++ MyGuitarTunner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyGuitarTunner
+//
+//  Created by Danny Vaca on 16/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyGuitarTunner/MyGuitarTunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGuitarTunnerApp.swift
+//  MyGuitarTunner
+//
+//  Created by Danny Vaca on 16/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MyGuitarTunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
